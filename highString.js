60d1ee37902dc7be5860ab6ec0a70e213c8c06e2
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-11-07 00:26:39 +0100

    Update highString.js

--- highString.js
+++ highString.js
@@ -1,9 +1,8 @@
 function longestChar(s) {
     let count = 0;
-    let highstCharOccurns = null;
     let map = {}
     
-        if ( !(1 <= s.length && s.length <= 100) ) return 'string length: ' + s.length + ' is out of bound';
+    if ( !(1 <= s.length && s.length <= 100) ) return 'string length: ' + s.length + ' is out of bound';
       let formatStr = s.replace(/[^a-zA-Z]/g, "");
       
       formatStr.split('').map((char) => {
@@ -17,15 +16,56 @@ function longestChar(s) {
           map[char] = count;
         }
         
-      })
+    })
+
       let values = Object.values(map);
       let keys = Object.keys(map);
       let highIndex = Object.values(map).indexOf(Math.max(...values ))
       
-      console.log( keys[highIndex] )
-      
-      return map
-    }
+      return keys[highIndex]
+}
+
+function longestChar2(s) {
+  let count = 0;
+  let map = {}
+  
+  if ( !(1 <= s.length && s.length <= 100) ) return 'string length: ' + s.length + ' is out of bound';
+    let formatStr = s.replace(/[^a-zA-Z]/g, "");
     
-    const str = "aakiAKDNEKSKSKNEJDNSJKLSNKWLMAKDOWMFCLKSsassyhwywjsgagkcdj1678293"
-    console.log(longestChar(str))
+    formatStr.split('').map((char) => {
+        if(map[char]){
+          count = map[char];
+        count++;
+        map[char] = count
+      }
+        if(map[char] === undefined){
+          count = 1;
+        map[char] = count;
+      }
+      
+  })
+    let keys = Object.keys(map);
+    let highCount = 0;
+    let highest = "";
+
+
+    keys.map((key) => {
+      if(map[key] > highCount) {
+        highCount = map[key];
+        highest = key
+      }
+    })
+    
+    return highest;
+}
+    
+const str = "aakiAKkjkdnjcdvvjvwblnncnJNVNSLBDIHEWUWDCNuhfdnjxmDNEKSKSKNEJDNSJKLSNKDDDDDWLMAKDOWMFCLKSsassyhwyw"
+// const str = "AAAihf78"
+console.log(str.length)
+console.time("sol 1")
+console.log(longestChar(str))
+console.timeEnd("sol 1")
+
+console.time("sol 2")
+console.log(longestChar2(str))
+console.timeEnd("sol 2")
