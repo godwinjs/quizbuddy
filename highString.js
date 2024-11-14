014f2f1fc4dc40cca20f029fb1ed954a789a29b4
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-11-14 11:56:07 +0100

    Update highString.js

--- highString.js
+++ highString.js
@@ -162,7 +162,7 @@ function smallestValue(n) {
   }
 } 
 // console.log(smallestValue(9))
-console.log(primeFactors(15))
+// console.log(primeFactors(15))
 // console.log(unFactorable(2))
 
 function primeFactors(num){
@@ -192,3 +192,48 @@ function primeFactors(num){
 
 //   return n
 // }
+
+function longChar(s) {
+  let count = 0;
+  let map = {}
+  
+  if ( !(1 <= s.length && s.length <= 100) ) return 'string length: ' + s.length + ' is out of bound';
+    let formatStr = s.replace(/[^a-zA-Z]/g, "");
+    
+    formatStr.split('').map((char) => {
+        if(map[char]){
+          count = map[char];
+        count++;
+        map[char] = count
+      }
+        if(map[char] === undefined){
+          count = 1;
+        map[char] = count;
+      }
+      
+  })
+
+    let values = Object.values(map);
+    let keys = Object.keys(map);
+    let highIndex = Object.values(map).indexOf(Math.max(...values ))
+    
+    console.log( keys[highIndex] )
+    
+    return map
+}
+  
+// const str = "aakiAKDNEKSKSKNEJDNSJKLSNKWLMAKDOWMFCLKSsassyhwywjsgagkcdj1678293"
+// console.log(longestChar(str))
+
+function longestAlpha(str) {
+const map = {};
+
+  for (let i = 0; i < str.length; i++ ){
+    if(/a-zA-Z/.test()){
+      console.log(str[i])
+    }
+  }
+}
+
+
+console.log(longestAlpha('HHHty7382LL;;'), 'runinf')
